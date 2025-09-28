57a5b94f670a9097fb6d581b2d2840944674a4a0
LeonMueller1208 <leonuni1208@gmail.com>
2025-09-29 01:03:43 +0200

    Fix syntax error in company page

--- web/app/company/page.tsx
+++ web/app/company/page.tsx
@@ -191,7 +191,6 @@ export default function CompanyDashboard() {
                     </button>
                   </div>
                 </div>
-                </div>
               </div>
               ))}
             </div>
